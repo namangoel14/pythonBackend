fee5d5ca17ce513edd9ce4957b547d76fba61059
Naman Goel <naman.goel@amantyatech.com>
2026-06-15 12:51:17 +0530

    Chapter07 of loops

---
+++ pyFiles/Chapter07/while_loop.py
@@ -0,0 +1,22 @@
+i = 1
+
+while (i<=50):
+    print(i)
+    if i == 35:
+        print("Yay its 35!")
+        break
+    i += 1
+
+# Write a program to print the content of a list using while loops.
+
+list_content = ["Hi I am Naman", "I am feeling good", "I am learning python", "I am becoming good human being"]
+
+i=0
+while (i < len(list_content)):
+    print(list_content[i])
+    i +=1
+
+
+# 4. Write a program to find whether a given number is prime or not.
+
+
